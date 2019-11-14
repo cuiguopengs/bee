cf523a5645430683d4c904340b6e16f218f38d93
cuiguopeng <cuiguopeng@hotmail.com>
2019-11-14 23:31:05 +0800

    增加ribbon测试文件

---
+++ ribbon/src/main/java/com/bee/ribbon/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.bee.ribbon.controller;
+
+import com.bee.ribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String hi(@RequestParam(value = "name") String name) {
+        return helloService.hiService(name);
+    }
+
+}
