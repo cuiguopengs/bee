6a0ac8c09180ee65593d573926a7ba1374944641
cuiguopeng <cuiguopeng@hotmail.com>
2019-11-14 19:47:04 +0800

    no message

---
+++ src/test/java/com/bee/cloud/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bee.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
