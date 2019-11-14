4faad16c58b9ff698d282d3738324683f93ac5c6
cuiguopeng <cuiguopeng@hotmail.com>
2019-11-14 22:38:55 +0800

    增加测试文件

---
+++ client1/src/main/java/com/bee/client1/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.bee.client1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping(value = "/hi")
+    public String home(@RequestParam String name) {
+        System.out.println("名字：" + name + "__PORT:" + port);
+        return "名字：" + name + "__PORT:" + port;
+    }
+}
