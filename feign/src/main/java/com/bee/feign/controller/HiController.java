f790aaf7744dae4a302a1f9889eaf38300a77d85
cuiguopeng <cuiguopeng@hotmail.com>
2019-11-14 23:08:41 +0800

    增加测试文件

---
+++ feign/src/main/java/com/bee/feign/controller/HiController.java
@@ -0,0 +1,23 @@
+package com.bee.feign.controller;
+
+import com.bee.feign.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam(value = "name") String name) {
+        System.out.println("feign has run once");
+        return schedualServiceHi.sayHiFromClientOne(name);
+
+    }
+
+}
