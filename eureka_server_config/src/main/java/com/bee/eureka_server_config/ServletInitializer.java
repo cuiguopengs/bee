2f2a5f185deef942d5d29a93738d4871f46635a4
cuiguopeng <cuiguopeng@hotmail.com>
2019-11-16 14:57:42 +0800

    增加高可用的分布式配置中心

---
+++ eureka_server_config/src/main/java/com/bee/eureka_server_config/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bee.eureka_server_config;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EurekaServerConfigApplication.class);
+    }
+
+}
